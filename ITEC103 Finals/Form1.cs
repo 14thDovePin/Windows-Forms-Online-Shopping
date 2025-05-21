c5a5f5046bd9b2d474bed2c6369dbf37434d8840
14thDovePin <therlaf0409@gmail.com>
2025-05-21 18:08:58 +0800

    created separate files for logic for each panels.

--- ITEC103 Finals/Form1.cs
+++ ITEC103 Finals/Form1.cs
@@ -1,6 +1,3 @@
-using System.Net.Sockets;
-using System.Windows.Forms;
-
 namespace ITEC103_Finals
 {
     public partial class Form1 : Form
---
+++ ITEC103 Finals/PanelsCode/CartPanel.cs
@@ -0,0 +1,8 @@
+﻿namespace ITEC103_Finals
+{
+    public partial class Form1 : Form
+    {
+        // Add Code Here
+
+    }
+}
